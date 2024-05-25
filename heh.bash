011f200d65aa7a505038fa4bc2e5328d0bc4c05e
Danny1234561111 <shornikovdaniil1111@gmail.com>
2024-05-25 02:47:08 +0000

    write comment in file

--- heh.bash
+++ heh.bash
@@ -1,6 +1,8 @@
 #!/bin/bash
 file_to_backup="myfile.txt"
+echo "Enter comment in file:"
+read comment
 backup_file="backup_$(date +'%Y%m%d%H%M%S').txt"
 cp "$file_to_backup" "$backup_file"
 echo "Backup created: $backup_file"
-
+echo "# $comment #">>$backup_file
